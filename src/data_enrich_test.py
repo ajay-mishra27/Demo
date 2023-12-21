f10742f95244a0090fd403f617c3b682c5e2aaea
Bhanu Chandra <obhanucse2014@gmail.com>
2023-12-21 11:45:04 -0800

    Changes regarding the row insertion

--- src/data_enrich_test.py
+++ src/data_enrich_test.py
@@ -1,19 +1,32 @@
 import sys
-from services.data_enrichment_service import DataEnrichment  
-from utils.sql_connection import SQLConnection
+from services.data_enrichment_service import DataEnrichment 
+from utils.sql_connection import SQLConnection 
+import pyodbc 
 import json
 
-if __name__ == "__main__":
+def transform_enhance_data(row_id, simulation=False):
+    print("File enrichment started")
     try:
-        print("MT103 file Conversion started")
+        print("File transaformation started")
         process = DataEnrichment()
-        sql_class = SQLConnection()
-        sql_class.get_connection()
-        input_json = sql_class.get_pipeline_config()
-        dataframes = sql_class.get_kafka_messages()
+        sqlConnection = SQLConnection()
+        dataframes = []
+        sqlConnection.get_connection()
+        results = sqlConnection.get_kafka_messages(row_id)
+        input_json = sqlConnection.get_pipeline_config(row_id)
+        pipeline_id = 0
+        for row in results:
+            dataframes.append(json.loads(row['message']))
+            pipeline_id = row['pipeline_config_id']
+
+        
+        
+        print(dataframes)
         json_arryay_enrich = []
         json_arryay_transfarmeres = []
+        
         for each_json in input_json:
+            print(each_json["pipelineName"])
             configuration_json = each_json['config']['configuration']
             for each_config_json in configuration_json:
                 message_variants_json = each_config_json['source_kde']['Message_variants']
@@ -24,15 +37,26 @@ if __name__ == "__main__":
                             json_arryay_enrich.append(each_json)
                         for each_json in pipeline_operators_json['transformer']['rows']:
                             json_arryay_transfarmeres.append(each_json)
-        print(dataframes)
         dataframes = process.perform_data_enrichment(dataframes,json_arryay_enrich)
         print("Date after enrichment")
         print(dataframes)
         dataframes = process.perform_data_transform(dataframes,json_arryay_transfarmeres)
         print(dataframes)
-        with open("test.txt",'w') as file:
-            json.dump(dataframes,file,ensure_ascii=False)
-        print("MT103 File conversion Ended") 
+        dump_row_id = ""
+        for eachJson in dataframes:
+            query = f'insert into dbo.t_process_message_dump (id, message, process_status, pipeline_config_id) values (\'{row_id}\',\'{json.dumps(eachJson)}\',\'success\',\'{pipeline_id}\');'
+            dump_row_id = sqlConnection.execute_query(query)
+        # with open("test.txt",'w') as file:
+        #     json.dump(dataframes,file,ensure_ascii=False)
+        print(f"File Transformation Ended {dump_row_id}") 
+        sqlConnection.close_db_connection()
+        return(row_id)
+        
     except Exception as exp:
         print("Excetion")
+        #print(exp.message)
         raise(exp)
+    
+if __name__ == "__main__":
+    transform_enhance_data(42)
+    
--- src/utils/sql_connection.py
+++ src/utils/sql_connection.py
@@ -16,46 +16,67 @@ class SQLConnection:
         connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
         self.conn = pyodbc.connect(connectionString) 
 
-    def get_pipeline_config(self):
+    def get_pipeline_config(self,row_id):
         results = []
         fresults = []
         cursor = self.conn.cursor()
-        cursor.execute("""
-                            SELECT TOP (10) [pipeline_config_id]
-                ,[pipeline_name]
-                ,[json_message]
-                ,[created_by]
-                ,[created_at]
-                ,[updated_by]
-                ,[updated_at]
-            FROM [paymentProcessing].[dbo].[t_pipeline_config] where pipeline_config_id=1
-            """)
+        cursor.execute(f"""
+                                SELECT [pipeline_config_id]
+            ,[pipeline_name]
+            ,[json_message]
+            ,[created_by]
+            ,[created_at]
+            ,[updated_by]
+            ,[updated_at]
+        FROM [paymentProcessing].[dbo].[t_pipeline_config] 
+        where pipeline_config_id=(select pipeline_config_id from paymentProcessing.dbo.t_kafka_raw_dump tkrd where id={row_id})
+                """)
         columns = [column[0] for column in cursor.description]
         result = cursor.fetchall()
         for row in result:
             results.append(dict(zip(columns, row)))
         for row in results:
             fresults.append(json.loads(row['json_message']))
-        return fresults   
+        cursor.close()
+        return fresults
+           
 
-    def get_kafka_messages(self):
+    def get_kafka_messages(self,row_id):
         results = []
-        fresults = []
         cursor = self.conn.cursor()
-        cursor.execute("""
-                                    SELECT TOP (10) [id]
+        cursor.execute(f"""
+                                    SELECT [id]
                         ,[message]
                         ,[created_at]
                         ,[pipeline_config_id]
                         ,[file_name]
                     FROM [paymentProcessing].[dbo].[t_kafka_raw_dump]
-
-                    where pipeline_config_id=1
+                    where id={row_id}
             """)
         columns = [column[0] for column in cursor.description]
         result = cursor.fetchall()
         for row in result:
             results.append(dict(zip(columns, row)))
-        for row in results:
-            fresults.append(json.loads(row['message']))
-        return fresults
+        cursor.close()
+        return results
+    
+    def close_db_connection(self):
+        self.conn.close()
+
+
+    def execute_query(self,query):
+        cursor =  self.conn.cursor()
+        if query.startswith("select"):
+            cursor.execute(query)
+            rows = cursor.fetchall()
+        else:
+            rows = []
+            cursor.execute(query)
+            # Get the id of the executed insert query
+            new_id_query = "SELECT SCOPE_IDENTITY();"
+            cursor.execute(new_id_query)
+            row_id = cursor.fetchone()[0]
+            rows.append(str(row_id))
+            cursor.execute("Commit;")
+        return rows
+        
